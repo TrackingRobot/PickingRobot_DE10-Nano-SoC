046d8caa2c7cfdfb84c9924a4e4e00907130c027
TrackingRobot <371631846@qq.com>
2023-10-20 15:24:58 +0800

    add docments and hw files

---
+++ hw/DE10_NANO_SoC_GHRD_D8M/DE10_SOC_GPIO0.tcl
@@ -0,0 +1,107 @@
+# Copyright (C) 1991-2013 Altera Corporation
+# Your use of Altera Corporation's design tools, logic functions 
+# and other software and tools, and its AMPP partner logic 
+# functions, and any output files from any of the foregoing 
+# (including device programming or simulation files), and any 
+# associated documentation or information are expressly subject 
+# to the terms and conditions of the Altera Program License 
+# Subscription Agreement, Altera MegaCore Function License 
+# Agreement, or other applicable license agreement, including, 
+# without limitation, that your use is for the sole purpose of 
+# programming logic devices manufactured by Altera and sold by 
+# Altera or its authorized distributors.  Please refer to the 
+# applicable agreement for further details.
+
+# Quartus II 64-Bit Version 13.1.0 Build 162 10/23/2013 SJ Full Version
+# File: C:\altera\Quartus_project\DE1_SOC_golden_top.tcl
+# Generated on: Sat May 18 20:42:03 2019
+
+package require ::quartus::project
+
+
+
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to GPIO[0]
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to GPIO[1]
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to GPIO[2]
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to GPIO[3]
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to GPIO[4]
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to GPIO[5]
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to GPIO[6]
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to GPIO[7]
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to GPIO[8]
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to GPIO[9]
+
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to GPIO[10]
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to GPIO[11]
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to GPIO[12]
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to GPIO[13]
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to GPIO[14]
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to GPIO[15]
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to GPIO[16]
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to GPIO[17]
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to GPIO[18]
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to GPIO[19]
+
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to GPIO[20]
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to GPIO[21]
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to GPIO[22]
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to GPIO[23]
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to GPIO[24]
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to GPIO[25]
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to GPIO[26]
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to GPIO[27]
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to GPIO[28]
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to GPIO[29]
+
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to GPIO[30]
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to GPIO[31]
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to GPIO[32]
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to GPIO[33]
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to GPIO[34]
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to GPIO[35]
+
+
+
+set_location_assignment PIN_Y15 -to GPIO[0]
+set_location_assignment PIN_AC24 -to GPIO[1]
+set_location_assignment PIN_AA15 -to GPIO[2]
+set_location_assignment PIN_AD26 -to GPIO[3]
+set_location_assignment PIN_AG28 -to GPIO[4]
+set_location_assignment PIN_AF28 -to GPIO[5]
+set_location_assignment PIN_AE25 -to GPIO[6]
+set_location_assignment PIN_AF27 -to GPIO[7]
+set_location_assignment PIN_AG26 -to GPIO[8]
+set_location_assignment PIN_AH27 -to GPIO[9]
+
+set_location_assignment PIN_AG25 -to GPIO[10]
+set_location_assignment PIN_AH26 -to GPIO[11]
+set_location_assignment PIN_AH24 -to GPIO[12]
+set_location_assignment PIN_AF25 -to GPIO[13]
+set_location_assignment PIN_AG23 -to GPIO[14]
+set_location_assignment PIN_AF23 -to GPIO[15]
+set_location_assignment PIN_AG24 -to GPIO[16]
+set_location_assignment PIN_AH22 -to GPIO[17]
+set_location_assignment PIN_AH21 -to GPIO[18]
+set_location_assignment PIN_AG21 -to GPIO[19]
+
+set_location_assignment PIN_AH23 -to GPIO[20]
+set_location_assignment PIN_AA20 -to GPIO[21]
+set_location_assignment PIN_AF22 -to GPIO[22]
+set_location_assignment PIN_AE22 -to GPIO[23]
+set_location_assignment PIN_AG20 -to GPIO[24]
+set_location_assignment PIN_AF21 -to GPIO[25]
+set_location_assignment PIN_AG19 -to GPIO[26]
+set_location_assignment PIN_AH19 -to GPIO[27]
+set_location_assignment PIN_AG18 -to GPIO[28]
+set_location_assignment PIN_AH18 -to GPIO[29]
+
+set_location_assignment PIN_AF18 -to GPIO[30]
+set_location_assignment PIN_AF20 -to GPIO[31]
+set_location_assignment PIN_AG15 -to GPIO[32]
+set_location_assignment PIN_AE20 -to GPIO[33]
+set_location_assignment PIN_AE19 -to GPIO[34]
+set_location_assignment PIN_AE17 -to GPIO[35]
+
+
+
+
